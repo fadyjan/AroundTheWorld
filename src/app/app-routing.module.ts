c759b7e065ce65eebe56433eb32d4e83cda7378a
fadyjan <fady.jan95@gmail.com>
2021-09-30 06:09:48 +0200

    Create #8 Control Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginBoxComponent } from './login-box/login-box.component';
+import { CoutriesComponent } from './coutries/coutries.component';
+import { CitiesComponent } from './cities/cities.component';
+const routes: Routes = [
+  { path: '', component: LoginBoxComponent },
+  { path: 'AllCountries', component: CoutriesComponent },
+  {path :'country/:id', component:CitiesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
